7618b065a245fd07db29b5c8692dc0ebc52d3944
Roberto Nicoletti <roberto.urias@gmail.com>
2016-09-27 14:56:37 -0300

    MAterial Design, Jquery-Autocompete, Parallax

---
+++ JQuery-Autocomplete/init.js
@@ -0,0 +1,41 @@
+$(function(){
+    var countries = [
+        { value: 'Andorra', data: 'AD' },
+        { value: 'Afeganistão', data: 'AF' },
+        { value: 'Africa do Sul', data: 'ZA' },
+        { value: 'Alemanha', data: 'DE' },
+        { value: 'Angola', data: 'AO' },
+        { value: 'Antartica', data: 'AQ' },
+        { value: 'Argentina', data: 'AR' },
+        { value: 'Austrália', data: 'AU' },
+        { value: 'Brasil', data: 'BR' },
+        { value: 'Hong-Kong', data: 'HK' },
+        { value: 'Irlanda', data: 'IE' },
+        { value: 'Japão', data: 'JP' },
+        { value: 'Zimbabwe', data: 'ZZ' }
+    ];
+
+    var countries = [
+        { value: 'Andorra'},
+        { value: 'Afeganistão'},
+        { value: 'Africa do Sul'},
+        { value: 'Alemanha'},
+        { value: 'Angola'},
+        { value: 'Antartica'},
+        { value: 'Argentina'},
+        { value: 'Austrália'},
+        { value: 'Brasil'},
+        { value: 'Hong-Kong'},
+        { value: 'Irlanda'},
+        { value: 'Japão'},
+        { value: 'Zimbabwe' }
+    ];
+
+    $('#autocomplete').autocomplete({
+        lookup: countries_sigle,
+        onSelect: function (suggestion) {
+            alert('Sua seleção: ' + suggestion.value + ', ' + suggestion.data);
+        }
+    });
+
+});
